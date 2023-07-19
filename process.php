1b3eeb366276c11bd0440beaa36ede341e8c766a
icemoneyglobal <139908088+icemoneyglobal@users.noreply.github.com>
2023-07-19 13:41:30 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
     $server = 'localhost'; // 127.0.0.1
     $username = 'root';
-    $password = 'rootroot';
+    $password = '';
     $db = 'csc309';
 
     // Open a new connection
@@ -13,12 +13,15 @@
     }
 
     // Query
+    if(isset($_POST['submit'])){ 
     $firstname = $_POST['firstname'];
     $lastname = $_POST['lastname'];
     $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    $date_of_birth = $_POST['date_of_birth'];
 
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('$firstname', '$lastname', '$email')";
+    $sql = "INSERT INTO users (firstname, lastname, email, gender, date_of_birth) 
+    VALUES ('$firstname', '$lastname', '$email', '$gender', '$date_of_birth')";
 
     $result = $con->query($sql);
 
@@ -27,6 +30,7 @@
     } else {
         echo 'Error inserting record: ' . $con->error;
     }
+}
 
     // Close connection
     $con->close();
