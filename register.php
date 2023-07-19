b8fda6a152ecca60e07ab590e438c96ff04ef3b2
icemoneyglobal <139908088+icemoneyglobal@users.noreply.github.com>
2023-07-19 13:45:13 +0100

    Update register.php

--- register.php
+++ register.php
@@ -21,8 +21,17 @@
             <label for="email">Email:</label>
             <input type="email" name="email">
         </div>
-
-        <button type="submit">Submit</button>
+        <div>
+            <label for="gender">Gender:</label>
+            <input type="radio" name="gender" value = "male" required> male
+            <input type="radio" name="gender" value = "female" required> female
+            <input type="radio" name="gender" value = "other" required> other
+        </div>
+        <div>
+            <label for="date of birth">date of birth:</label>
+            <input type="date" name="date_of_birth">
+        </div>
+        <button type="submit" name="submit">Submit</button>
     </form>
 </body>
 </html>
